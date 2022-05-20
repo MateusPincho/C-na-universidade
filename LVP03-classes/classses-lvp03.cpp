98e09a47e4add8494a84e1c46cd13667e164187f
Mateus Pincho <mateuspoliveira.ee@gmail.com>
2022-05-20 12:02:26 -0300

    Finalização LVP03 - sofri mas foi bom

--- LVP03-classes/classses-lvp03.cpp
+++ LVP03-classes/classses-lvp03.cpp
@@ -20,39 +20,41 @@ using namespace std;
 
 struct Resistor // tipo resistor 
 {
-    float ddpCircuito; // recebe entradaddp
-    float correnteCircuito; // recebe entradacorrente
-    float resistenciaCircuito; 
-
-    float ddpResistor;
-    float correnteResistor;
-    float resistenciaResistor;
+    
+    float ddp;
+    float corrente;
+    float resistencia;
+    float potencia;
+    float condutancia;
 
 };
-struct Capacitor
+struct Capacitor // tipo capacitor
 {
-    float cargaCircuito;
-    float moduloddp; // recebe entradaddp
-    float capacitanciaCapacitor;
+    float carga;
+    float ddpCapacitor; 
+    float capacitancia;
 
 };
-
 
 class circuito
 {
-    Resistor ResistenciaCircuito1;
-    Resistor ResistenciaCircuito2;
-    Capacitor CapacitanciaCircuito;
+    Resistor ResistorCircuito1;
+    Resistor ResistorCircuito2;
+    Capacitor CapacitorCircuito;
 
     public:
-    void setCircuito(float,float);
-    void setResistor(float, float, float);
-    float getResistenciaCircuito();
-    float getResistenciaSerie(float,int);
-    float getResistenciaParalelo(float,int);
-    float getTensao(int);
-    float getCorrente(int);
-    float getPotencia();
+    void setCircuito(float,float,float,float,float,float);
+    float getResistencia1();
+    float getResistencia2();
+    float getPotencia1();
+    float getPotencia2();
+    float getCondutancia1();
+    float getCondutancia2();
+    float getResistoresSerie();
+    float getResistoresParalelo();
+    float getCapacitancia();
+    float getEnergiaPot();
+    
 };
 
 
@@ -63,124 +65,186 @@ int main() // Programa Principal - Simulador
 
     // Passo 1 - Criar circuito simulado
 
-    circuito SimuladorCircuitoEletrico;  // Criando circuito simulado
+    circuito SimuladorCircuitoEletrico;  
 
     cout << "---------------SIMULADOR DE CIRCUITO ELÉTRICO---------------" << endl
-         << "Você criou um circuito fechado com dois resistores iguais e um capacitor!" << endl;
+         << "Você criou um circuito fechado com dois resistores e um capacitor!" << endl;
 
-    // Passo 2 - Receber os valores de tensão e corrente do circuito
+    // Passo 2.1 - Receber os valores de tensão e corrente dos resistores 1 e 2
 
-    float entradaddp;   
-    float entradacorrente;
+    float entradaddp1;   
+    float entradacorrente1;
+    float entradaddp2;   
+    float entradacorrente2;
 
-    cout << "Informe o valor da tensão do circuito: ";
-    cin >> entradaddp;  // Usuário informa ddp
-    cout << "Informe o valor da corrente do circuito: ";
-    cin >> entradacorrente; // Usuário informa corrente
+    cout << "Para o Resistor (1): " << endl
+         << "Informe o valor da ddp que passa por este resistor : ";
+    cin >> entradaddp1; 
+
+    cout << "Informe o valor da corrente que passa por este resistor : ";
+    cin >> entradacorrente1; 
+
+    cout << "Para o Resistor (2): " << endl
+         << "Informe o valor da ddp que passa por este resistor : ";
+    cin >> entradaddp2; 
+
+    cout << "Informe o valor da corrente que passa por este resistor : ";
+    cin >> entradacorrente2; 
+
+    //Passo 2.2 - Receber os valores de tensão e carga do capacitor
+
+    float entradaddpC;
+    float entradacarga;
+
+    cout << "Para o Capacitor : " << endl
+         << "Informe o valor da ddp que passa pelo capacitor : ";
+    cin >> entradaddpC; 
+
+    cout << "Informe a quantidade de carga que este capacitor armazena : ";
+    cin >> entradacarga;
+
 
     // Passo 3 - armazenar as configurações do circuito
 
-    SimuladorCircuitoEletrico.setCircuito(entradaddp,entradacorrente); 
+    SimuladorCircuitoEletrico.setCircuito(entradaddp1,entradacorrente1,entradaddp2,entradacorrente2,entradaddpC,entradacarga);
+   
+    // Passo 4 - Iniciar análise do circuito 
 
-    // Passo 4 - Achar a Resistência do circuito = Resistencia Equivalente
+    cout << "Você acabou de configurar o seu Circuito Simulado!" << endl<< " Vamos agora fazer uma análise minuciosa sobre suas caracteristicas" << endl;
+    system("pause");
 
-    float resistenciaTotal = SimuladorCircuitoEletrico.getResistenciaCircuito();
-    cout << "A resistência total do circuito é: " << fixed << setprecision(2) << resistenciaTotal << " Ohms" << endl;
+    // Passo 5 - Achar as informações dos resistores
 
-    // Passo 5 - Saber se os resistores estão em série ou paralelo
+    //Para o primeiro resistor (1)
 
-    int tipocircuito;
+    cout << "A tensão que passa pelo resistor (1) é" 
+         << fixed << setprecision(2) << entradaddp1 << " Volts" << endl;
+    cout << "A corrente que passa pelo resistor (1) é" 
+         << fixed << setprecision(2) << entradacorrente1 << " Amper" << endl;
+    cout << "A resistência do resistor (1) é " 
+         << fixed << setprecision(2) << SimuladorCircuitoEletrico.getResistencia1() << " Ohms" << endl;
+    cout << "A Potência do resistor (1) é " 
+         << fixed << setprecision(2) << SimuladorCircuitoEletrico.getPotencia1() << " Watts" << endl;
+    cout << "A condutância do resistor (1) é "
+         << fixed << setprecision(3) << SimuladorCircuitoEletrico.getCondutancia1() << " Siemens" << endl << endl;
+    system("pause");
+    // Para o segundo resistor (2)
 
-    cout << "Agora é com você!" << endl
-         << "Os resistores do seu circuito estão em série [1] ou em paralelo [2]? ";
-    cin >> tipocircuito;
+    cout << "A tensão que passa pelo resistor (2) é " 
+         << fixed << setprecision(2) << entradaddp2 << " Volts" << endl;
+    cout << "A corrente que passa pelo resistor (2) é " 
+         << fixed << setprecision(2) << entradacorrente2 << " Amper" << endl;
+    cout << "A resistência do resistor (2) é " 
+         << fixed << setprecision(2) << SimuladorCircuitoEletrico.getResistencia2() << " Ohms" << endl;
+    cout << "A Potência do resistor (2) é " 
+         << fixed << setprecision(2) << SimuladorCircuitoEletrico.getPotencia2() << " Watts" << endl;
+    cout << "A condutância do resistor (2) é "
+         << fixed << setprecision(3) << SimuladorCircuitoEletrico.getCondutancia2() << " Siemens" << endl << endl;
+    system("pause");
 
-    // Passo 6 - Calcular o valor da resistência de cada resistor
-
-    float resistor; // Armazena a resistência de 1 resistor
-
-    switch (tipocircuito)
-    {
-    case 1:      // Circuito em série
-        resistor = SimuladorCircuitoEletrico.getResistenciaSerie(resistenciaTotal,2);
-        cout << "Cada resistor possui " << fixed << setprecision(2) << resistor << "Ohms" << endl;
-        break;
-
-    case 2:     // Circuito em paralelo
-        resistor = SimuladorCircuitoEletrico.getResistenciaParalelo(resistenciaTotal,2);
-        cout << "Cada resistor possui " << fixed << setprecision(2) << resistor << "Ohms" << endl;
-        break;
+    // Passo 6 - Para os resistores associados
     
-    default:
-        cout << "Você digitou uma opção inválida, vamos ter que recomeçar :(";
-        break;
-    }
+    // Em Série
+
+    cout << "A resistência equivalente do circuito caso os resistores estivem ligados em Série é "
+         << fixed << setprecision(2) << SimuladorCircuitoEletrico.getResistoresSerie() << " Ohms" << endl;
     
-    // Passo 7 - Calcular a tensão que passa por 1 resistor
+    // Em Paralelo
+    
+    cout << "A resistência equivalente do circuito caso os resistores estivem ligados em Paralelo é "
+         << fixed << setprecision(2) << SimuladorCircuitoEletrico.getResistoresParalelo() << " Ohms" << endl << endl;
 
-    float tensaoPorResistor = SimuladorCircuitoEletrico.getTensao(tipocircuito);
-    cout << "Por cada um dos resistores está passando uma tensão de " << fixed << setprecision(2) << tensaoPorResistor << "volts" << endl;
+    system("pause");
+    
+    // Passo 7 - Achar as informações do capacitor
 
-    // Passo 8 - Calcular a corrente que passa por 1 resistor
+    cout << "A tensão que passa pelo capacitor é " 
+         << fixed << setprecision(2) << entradaddpC << " Volts" << endl;
+    cout << "A carga armazenada pelo capacitor é " 
+         << fixed << setprecision(2) << entradacarga << " Columbs" << endl;
+    cout << "A capacitância do capacitor é " 
+         << fixed << setprecision(2) << SimuladorCircuitoEletrico.getCapacitancia() << " Farad" << endl;
+    cout << "A Energia Potencial Eletrostática do capacitor é " 
+         << fixed << setprecision(2) << SimuladorCircuitoEletrico.getEnergiaPot() << " Joules" << endl;
 
-    float correntePorResistor = SimuladorCircuitoEletrico.getCorrente(tipocircuito);
-    cout << "Por cada um dos resistores está passando uma corrente de " << fixed << setprecision(2) << correntePorResistor << "amperes" << endl;
-
-    // Passo 9 - Armazenar todos os valores do resistor calculados 
-
-    SimuladorCircuitoEletrico.setResistor(resistor,tensaoPorResistor,correntePorResistor);
-
-    // Passo 10 - Calcular a potência do resistor
-
-
+    cout << "---------------FIM DA ANÁLISE---------------" << endl;
 
     return 0;
 }
 
-void circuito :: setCircuito(float numddp, float numcorrente)
+void circuito :: setCircuito(float ddpR1, float correnteR1, float ddpR2, float correnteR2, float ddpC, float carga )
 {
-    ResistenciaCircuito1.ddpCircuito = numddp;
-    ResistenciaCircuito1.correnteCircuito = numcorrente;
+    ResistorCircuito1.ddp = ddpR1;
+    ResistorCircuito2.ddp= ddpR2;
+    ResistorCircuito1.corrente =correnteR1;
+    ResistorCircuito2.corrente = correnteR2;
 
-    ResistenciaCircuito2.ddpCircuito = numddp;
-    ResistenciaCircuito2.correnteCircuito = numcorrente;
+    CapacitorCircuito.ddpCapacitor = ddpC;
+    CapacitorCircuito.carga =  carga;
+}
+
+// Métodos para o resistor (1)
+
+float circuito :: getResistencia1()
+{
+    ResistorCircuito1.resistencia = ResistorCircuito1.ddp / ResistorCircuito1.corrente;
+    return  ResistorCircuito1.resistencia;
+}
+
+float circuito :: getPotencia1()
+{
+   ResistorCircuito1.potencia = ResistorCircuito1.resistencia* pow (ResistorCircuito1.corrente,2);
+   return ResistorCircuito1.potencia;
+}
+
+float circuito :: getCondutancia1()
+{
+    ResistorCircuito1.condutancia = 1 / ResistorCircuito1.resistencia;
+    return ResistorCircuito1.condutancia;
+}
+
+// Métodos para o resistor (2)
+
+float circuito :: getResistencia2()
+{
+    ResistorCircuito2.resistencia = ResistorCircuito2.ddp / ResistorCircuito2.corrente;
+    return  ResistorCircuito2.resistencia;
+}
+
+float circuito :: getPotencia2()
+{
+   ResistorCircuito2.potencia = ResistorCircuito2.resistencia * pow (ResistorCircuito2.corrente,2);
+   return ResistorCircuito2.potencia;
+}
+
+float circuito :: getCondutancia2()
+{
+    ResistorCircuito2.condutancia = 1 / ResistorCircuito2.resistencia;
+    return ResistorCircuito2.condutancia;
+}
+
+// Métodos para encontrar a resistência equivalente
+
+float circuito :: getResistoresSerie()
+{
+    return ResistorCircuito1.resistencia + ResistorCircuito2.resistencia;
+}
+
+float circuito :: getResistoresParalelo()
+{
+    return (ResistorCircuito1.resistencia * ResistorCircuito2.resistencia) / (ResistorCircuito1.resistencia + ResistorCircuito2.resistencia);
+}
+
+// Métodos para o capacitor
+
+float circuito :: getCapacitancia ()
+{
+    CapacitorCircuito.capacitancia = CapacitorCircuito.carga / CapacitorCircuito.ddpCapacitor;
+    return CapacitorCircuito.capacitancia;
 
 }
 
-void circuito :: setResistor(float numresistor,float numtensaor, float numcorrenter)
+float circuito :: getEnergiaPot() 
 {
-    numresistor = ResistenciaCircuito1.resistenciaResistor;
-    numtensaor = ResistenciaCircuito1.ddpResistor;
-    numcorrenter = ResistenciaCircuito1.correnteCircuito;
-
-    numresistor = ResistenciaCircuito2.resistenciaResistor;
-    numtensaor = ResistenciaCircuito2.ddpResistor;
-    numcorrenter = ResistenciaCircuito2.correnteCircuito;
+    return pow(CapacitorCircuito.carga,2) / (2*CapacitorCircuito.capacitancia);
 }
-
-float circuito :: getResistenciaCircuito()
-{
-    // Tanto faz usar ResistenciaCircuito1 ou ResistenciaCircuito2 = possuem mesmos valores de circuito
-    return ResistenciaCircuito1.ddpCircuito / ResistenciaCircuito1.correnteCircuito;
-}
-
-float circuito :: getResistenciaSerie(float numRequivalente, int quantResistores )
-{
-    return numRequivalente / quantResistores; 
-}
-
-float circuito :: getResistenciaParalelo(float numRequivalente, int quantResistores)
-{
-    return numRequivalente * quantResistores;
-}
-
-float circuito :: getTensao(int tipo)
-{
-    return tipo == 1 ? (ResistenciaCircuito1.ddpCircuito/2) : (ResistenciaCircuito1.ddpCircuito);
-}
-
-float circuito :: getCorrente(int tipo)
-{
-    return tipo == 1? (ResistenciaCircuito1.correnteCircuito) : (ResistenciaCircuito1.correnteCircuito/2);
-}
-
